b78da988c83e80e0d583cf5e9dd419c03edc691b
elizabeth mathew20 <elizabethmatheweliza5@gmail.com>
2024-08-03 12:07:16 +0530

    first commit

---
+++ dictionary.py
@@ -0,0 +1,126 @@
+# v={"name":"arun","age":20,"sub":"maths","place":"alappuzha"}
+# print(v)
+# print(type(v))
+# print(len(v))
+# print(v["age"])
+
+# print(v.get("name"))
+# print(v.keys())
+# print(v.values())
+# print(v.items())
+
+# print("name" in v)
+
+# v["name"]="anu"
+# print(v)
+# v.update({"age":22})
+# print(v)
+# v["roll no"]=10
+# print(v)
+# v.update({"color":"red"})
+# print(v)
+# v.pop("name")
+# print(v)
+# v.popitem()
+# print(v)
+# del v["age"]
+# print(v)
+
+
+# v.clear()
+# print(v)
+
+# del v
+# print(v)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in v:
+#     print(i)
+# for i in v:
+#     print(v[i])    
+# for i in v.keys():
+#     print(i)
+# for i in v.items():
+#     print(i)
+# for i in v.values():
+#     print(i)    
+
+# c=v.copy()
+# print(c)    
+
+# c=dict(v)
+# print(c)
+
+
+# family={"child 1":{"name":"anu","age":21},"child 2":{"name":"manu","age":20},"child 3":{"name":"arun","age":22}}
+# print(family["child 2"])
+# print(family["child 2"]["age"])
+# print(family["child 3"]["name"])
+
+
+
+
+
+
+
+
+# school={"teach 1":{"name":"kiran","sub":"maths"},"teach 2":{"name":"manu","sub":"english"},"teach 3":{"name":"anu","sub":"hindi"}}
+# print(school["teach 2"])
+# print(school["teach 3"]["sub"])
+# print(school["teach 2"]["name"])
+# print(school["teach 1"]["name"])
+# print(school["teach 3"])
+# print(school["teach 2"]["sub"])
+
+
+
+
+
+
+
+
+
+
+
+a={"name":"riya","age":22,"height":178,"weight":40}
+# print(type(a))
+# print(len(a))
+
+# print(a["age"])
+# print(a["name"])
+
+# print(a.get("name"))
+# print(a.keys())
+# print(a.values())
+# print(a.items())
+
+
+# a.popitem()
+# print(a)
+
+
+# for i in a:
+#     print(i)
+# for i in a:   
+#     print(a[i]) 
+for i in a.keys():
+    print(i)
+for i in a.items():
+    print(i)
+for i in a.values():
+    print(i)    
